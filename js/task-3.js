aa05adb55a3913a100db39228e07cdd613d95085
Dmytro Holovskyi <80936904545dgold@gmail.com>
2024-05-13 17:35:58 +0300

    Update task-3 right now

--- js/task-3.js
+++ js/task-3.js
@@ -10,6 +10,18 @@ class StringBuilder {
   getValue() {
     return this.#value;
   }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
 }
 
 const builder = new StringBuilder('.');
